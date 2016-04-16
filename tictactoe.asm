940c54d3321b3440819230f1519493ebcf574fcc
Garrett <garrlker@hotmail.com>
2016-04-16 12:16:26 -0500

    Just pushing my updated code

--- tictactoe.asm
+++ tictactoe.asm
@@ -13,19 +13,23 @@ global main
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SECTION .data
 	;Prompts
+	cpuname:	db "Hal"
 	greetings:	db "Hello Human.", 10, "My name is Hal. What is yours?: ",0
 	shittalk:	db "Hello, %s, prepare to lose!", 10, 0
 	;Formats
 	strfmt:		db '%s',0
+	intfmt:		db '%d',0
 	;TicTacToe Grid
-	row1:		db "     |     |     ",10	;Will actually be Rows 1,2,4,5,7,8
-	row2:		db "     |     |     ",10	;Will actually be Rows 1,2,4,5,7,8
-	row3:		db "-----|-----|-----",10	;Will actually be Rows 3 and 6
-	row4:		db "     |     |     ",10	;Will actually be Rows 1,2,4,5,7,8
-	row5:		db "     |     |     ",10	;Will actually be Rows 1,2,4,5,7,8
-	row6:		db "-----|-----|-----",10	;Will actually be Rows 3 and 6
-	row7:		db "     |     |     ",10	;Will actually be Rows 1,2,4,5,7,8
+	row1:		db "     |     |     ",10	;For somereason, this won't draw with every row haves a null character	
+	row2:		db "     |     |     ",10	;Since they're after each other in memory doesn't hurt to do it this way
+	row3:		db "-----|-----|-----",10
+	row4:		db "     |     |     ",10
+	row5:		db "     |     |     ",10
+	row6:		db "-----|-----|-----",10
+	row7:		db "     |     |     ",10
 	finrow:		db "     |     |     ",10,0
+	;Misc
+	limit:		db 2				;
 	;Escape Sequences
 	;clearatr:	db 27,'(0',27,'[m',0;		;Until we need this let's leave it uncommented
 	clearscr:	db 27,'[H',27,'[2J',0;		;PrintF this to clear the screen between draws
@@ -36,16 +40,18 @@ SECTION .data
 	;Font Colors
 	gfont:		db 27,'[32m',0
 	rfont:		db 27,'[31m',0
-	wfont:		db 27,'[76m',0
+	wfont:		db 27,'[37m',0
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SECTION .bss
-	name:	resb	20;Reserve memory for user's name
+	name:		resb	20			;Reserve memory for user's name
+	terminal:	resb	9 			;One byte for each board position, (0=empty,1=Human,2=CPU)
+	random:  	resb	4			;Memory for our random value, who goes first
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 SECTION .text
 
 main:
 	call clearScreen	;Clear screen
-	call cyanback		;Set background color
+	call greenback		;Set background color
 	call whitefont
 	push dword greetings	;Push our message
 	call printf		;Print it
@@ -56,11 +62,19 @@ main:
 	call scanf		;Call the function
 	add esp,8		;Move the stack pointer
 
-	push name
-	push dword shittalk
-	call printf
-	add esp,8
+	push name		;Push user's name
+	push dword shittalk	;Let's shittalk
+	call printf		;Print to screen
+	add esp,8		;Move stack pointer
 	
+	call pickPlayer		;Let's pick who goes first
+
+	;push word [random]
+	;push intfmt
+	;push dword [random]
+	;call printf
+	;add esp,8
+
 	jmp Exit		;Exit our program
 	
 	clearScreen:		;Will clear the screen of everything
@@ -83,6 +97,29 @@ main:
 	add esp,36
 	ret
 
+	pickPlayer:
+	mov dword [random],0
+        push 0                  ;Push 0 for default time
+        call time               ;Seed random and generate number
+        add esp,4               ;Move stack pointer
+	push EAX                ;Push the time it returned
+        call srand              ;Now lets seed our random func with it
+        add esp,4               ;Move stack pointer
+        push EAX                ;Push so random will store in EAX
+        call rand               ;Let's get our random value
+        add esp,4               ;Move stack pointer
+        mov EBX,[limit]         ;Move 2 to EBX
+        xor EDX,EDX             ;Clear out EDX
+        div EBX                 ;Divide EAX by EBX remainder stored in EDX
+	mov [random],EDX	;Store random result in random (0 = cpu, 1 = human)
+	;push EDX
+	push strfmt
+	push word [random]
+	;push strfmt
+	call printf
+	add esp,8
+	ret
+
 	redback:		;Changes the cursor background to red
 	push dword redbackgrnd
 	call printf
@@ -91,18 +128,6 @@ main:
 	
 	greenback:		;Changes the cursor background to green
 	push dword greenbackgrnd
-	call printf
-	add esp,4
-	ret
-
-	cyanback:		;Changes the cursor background to cyan
-	push dword cyanbackgrnd
-	call printf
-	add esp,4
-	ret
-
-	redfont:
-	push dword rfont
 	call printf
 	add esp,4
 	ret
